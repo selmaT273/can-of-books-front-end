53ced0b45a88adc3c641172c5e4f7105d33058ba
Stacey Teltser <stacey.teltser@gmail.com>
2021-07-26 16:05:38 -0500

    added logout button to header and ensured its working

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import './Header.css';
+import LogoutButton from './LogoutButton';
 
 class Header extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class Header extends React.Component {
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
+        <LogoutButton />
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
       </Navbar>
     );
