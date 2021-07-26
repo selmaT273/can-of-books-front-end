f91b0d26920d6dcf0b73ff7f29d1305edd897699
Stacey Teltser <stacey.teltser@gmail.com>
2021-07-26 14:59:22 -0500

    wrapped app in auth0 provider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider 
+      domain="dev-24oeotkz.us.auth0.com" 
+      clientId="HKe1o6RtFddKPBVhimI2iljGBKnYRlBX" 
+      redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
