2106a3f04343648ceffff2a39b1adc9421c85be0
Stacey Teltser <stacey.teltser@gmail.com>
2021-07-26 21:18:49 -0500

    refactored to use class component and withAuth0 in Profile component using code from quickstart guide

--- src/Profile.js
+++ src/Profile.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import { withAuth0 } from "@auth0/auth0-react";
 
-const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+// code from auth0 quick start guide
+class Profile extends React.Component {
+  render(){
+    
+    const { user, isAuthenticated, isLoading } = this.props.auth0;
 
-  if (isLoading) {
-    return <div>Loading ...</div>;
+    if (isLoading) {
+      return <div>Loading ...</div>;
+    }
+
+    return (
+      isAuthenticated && (
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      )
+    );
   }
-
-  return (
-    isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
-    )
-  );
 };
 
-export default Profile;
+export default withAuth0(Profile);
