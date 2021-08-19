8260cd43c997497b42bbf6b38fdfc047c849a7bd
Stacey Teltser <stacey.teltser@gmail.com>
2021-08-18 19:36:27 -0500

    refactored to use componentDidMount and am getting response from local API in the console

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import axios from 'axios';
 
 class MyFavoriteBooks extends React.Component {
+    componentDidMount = () => {
+      axios.get(`http://localhost:3000/books`,
+      {params: {
+        email: 'booooo',
+      }})
+      .then(bookResponseData => {
+        console.log(bookResponseData);
+      })
+      .catch(err => console.log(err.message));
+    }
+
   render() {
     return(
+      <>
       <Jumbotron>
         <h1>My Favorite Books</h1>
         <p>
           This is a collection of my favorite books
         </p>
       </Jumbotron>
-    )
+      <div>
+      </div>
+      </>
+    );
   }
 }
 
